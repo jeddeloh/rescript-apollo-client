201116cf357064d90991e508489a4bac6e4581ca
Mariah Martinez <mariahmartinez13@gmail.com>
2020-11-18 15:05:50 -0700

    Add docusaurus doc site.

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  title: 'Rescript Apollo Client',
+  tagline: 'Rescript bindings for the Apollo Client ecosystem',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'reasonml-community', // Usually your GitHub org/user name.
+  projectName: 'rescript-apollo-client', // Usually your repo name.
+  themeConfig: {
+    colorMode: {
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: true,
+    },
+    navbar: {
+      title: 'Rescript Apollo Client',
+      logo: {
+        alt: 'Rescript Apollo Client Logo',
+        src: 'img/small_logo.png',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        { to: 'docs/changelog', label: 'Changelog', position: 'left' },
+        { to: 'docs/examples', label: 'Examples', position: 'left' },
+        { to: 'docs/contributing', label: 'Contributing', position: 'right' },
+        {
+          href: 'https://github.com/reasonml-community/reason-apollo-client',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} Reason Apollo Client. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/reasonml-community/reason-apollo-client/edit/master/website',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
